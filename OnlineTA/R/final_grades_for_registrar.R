b1467bb2fd67c450201998e514aa5273a07043b3
Brendan Apfeld <brendan.apfeld@utexas.edu>
2016-12-09 18:10:20 -0600

    Fix final grades script (multiple bugs)

--- OnlineTA/R/final_grades_for_registrar.R
+++ OnlineTA/R/final_grades_for_registrar.R
@@ -5,26 +5,24 @@
 #' @param class A character string of the abbreviated class name.  Default is GOV310L.  "GOV" must be in capital letters.
 #' @param non.enrolled A vector of character strings indicating the non-enrolled EIDs for the course.  Defaults are for speede, Tester McStudent, and Student McStudent.  Registrar allows no more than 3 non-enrolled eids in a course for the upload to succeed, so it may be necessary to add in student workers as well.
 #' @param points A vector of length 11 indicating the LOWER point values for each grade break A through D-.  Use only integers- the function will take care of rounding. Breaks do not need to be any particular order.
-#' @param round Should grades be rounded? If TRUE, then the final point total rounds according to standard mathematical rules and assigns letters based on that rounded score. If FALSE, then no rounding is performed.
-#' @param drop_decimal Logical depending on whether final grades should be based on only the whole number and drop any decimal values. I.e. an 88.97 is treated as an 88 and not an 89. This is an unusual setting. Defaults to FALSE. If set to TRUE, round must be FALSE.
+#' @param round Should grades be rounded? If set to "normal", then the final point total rounds according to standard mathematical rules and assigns letters based on that rounded score. If set to "floor," then final grades are based on only the whole number. I.e. an 88.97 is treated as an 88 and not an 89.
 #' @param full.out Should the TA/professor copy outputted include the entire gradebook or only the registrar version plus final points? Default is TRUE.
+#' @param final Should the "Final Points" or "Final Score" column be used to assign letter grades? Set to either "points" or "score".
 #' @return Outputs two files to a new folder in the working directory: "FinalGradeSubmission.txt" is the file that can be uploaded to the registrar; "Final Grades - Letters and Numbers.csv" is a copy for TA/professor records that includes point totals next to the grade letters.  Folder created is named "Final Grades -- Registrar Formatting"
 #' @examples
-#' final.grades(gradebook.file, class="GOV312L", points=c(487, 462, 451, 430, 412, 400, 392, 386, 350, 330, 312))
+#' final.grades(gradebook.file, class="GOV312L", points=c(487, 462, 451, 430, 412, 400, 392, 386, 350, 330, 312), final = "points")
 #' @export
 
-
-final.grades <- function(gradebook, class="GOV310L", non.enrolled=c("speede", "sm56684", "tm29778"), points=c(1:11), round = TRUE, drop_decimal = FALSE, full.out=TRUE){
+final.grades <- function(gradebook, class = "GOV310L", non.enrolled = c("speede", "sm56684", "tm29778"), points = c(94, 90, 87, 84, 80, 77, 74, 70, 67, 64, 60), round = c("normal", "floor"), full.out = TRUE, final = c("points", "score")){
   #check length of points vector
   if (length(points)!=11) stop("Points vector not of length 11")
-  if (drop_decimal == TRUE & round == TRUE) stop("drop_decimal and round cannot both be TRUE")
   #Are there any muted assignments? or the points possible row?
   elim <- c()
   if(mute_check(gradebook) == T){
-    elim <- 1
+    stop("Muted assignments detected - failed to export final grades.\n Muted assignments are not included in final grade calculations by Canvas. If you are confident your final points column is correct even with the muted assignments, you may ignore this message by deleting the muted assignment row (likely row 1) before you pass the gradebook to this function.")
   }
   if(points_possible_check(gradebook) == T){
-    elim <- c(elim, 2)
+    elim <- c(1)
   }
 
   #eliminate non-enrolled students and other top junk
@@ -32,39 +30,48 @@ final.grades <- function(gradebook, class="GOV310L", non.enrolled=c("speede", "s
   gradebook <- gradebook[-c(elim, elim_stud),]
   #column for unique number
   gradebook$unique <- as.numeric(substr(gradebook$Section, 9, 13))
+
   #if gb was imported using check.names=F, remove space and add period
-  names(gradebook) <- sub(" ", ".", names(gradebook))
-  #make sure final points is numeric
-  gradebook$`Final Points` <- as.numeric(as.character(gradebook$`Final Points`))
-  ## Dealing with grades
-  points <- sort(c(points, Inf, 0), decreasing=F)
-  if(drop_decimal == TRUE){
-    gradebook$Grade_Based_on_Points <- as.numeric(substr(gradebook$`Final Points`, 1,3))
-    gradebook$letter <- num_to_letter(gradebook$Grade_Based_on_Points, cutpoints = points, round = F)
+  names(gradebook) <- gsub(" ", ".", names(gradebook))
+
+  # Creating a vector of scores based on points or score
+  if(final %in% c("points", "point", "Points", "Point", "p", "P")){
+    gradebook$final_num <- suppressWarnings(as.numeric(as.character(gradebook$Final.Points)))
+  }else if(final %in% c("score", "scores", "Score", "score", "S", "s")){
+    gradebook$final_num <- suppressWarnings(as.numeric(as.character(gradebook$Final.Score)))
+  }else{
+    stop("You must set parameter 'final' to either 'points' or 'score' to indicate which column to use to assign final grades.\n Final grades not exported.")
   }
 
-  if(round == TRUE){
-    gradebook$Grade_Based_on_Points <- round(as.numeric(gradebook$`Final Points`))
+   ## Dealing with grades
+  points <- sort(c(points, Inf, 0), decreasing=F)
+  if(round %in% c("floor", "Floor", "f", "F")){
+    gradebook$Grade_Based_on_Points <- floor(gradebook$final_num)
+    gradebook$letter <- num_to_letter(gradebook$Grade_Based_on_Points, cutpoints = points, round = F)
+  } else if(round %in% c("normal", "Normal", "n", "N")){
+    gradebook$Grade_Based_on_Points <- round(gradebook$final_num)
     gradebook$letter <- num_to_letter(gradebook$Grade_Based_on_Points, cutpoints = points, round = T)
   } else{
-    gradebook$Grade_Based_on_Points <- as.numeric(gradebook$`Final Points`)
-    gradebook$letter <- num_to_letter(gradebook$Grade_Based_on_Points, cutpoints = points, round = F)
+    stop("It appears that you forgot to set the round variable. Please set it to either 'normal' for noraml mathematical rounding of grades, or 'floor' to use only the whole number.")
   }
 
   #Add columns for absences & remarks
   gradebook$absences <- NA
   gradebook$remarks <- NA
+
   #Keep only necessary columns
-  if (full.out==TRUE){
+  if (full.out == TRUE){
     grades <- gradebook
+  } else if(final %in% c("points", "point", "Points", "Point", "p", "P")){
+    grades <- subset(gradebook, select = c("Student", "SIS.User.ID", "letter", "absences", "remarks", "unique", "Final.Points"))
   } else{
-    grades <- subset(gradebook, select=c("Student", "SIS User ID", "letter", "absences", "remarks", "unique", "Final Points"))
+    grades <- subset(gradebook, select = c("Student", "SIS.User.ID", "letter", "absences", "remarks", "unique", "Final.Score"))
   }
-  gradebook <- subset(gradebook, select=c("Student", "SIS User ID", "letter", "absences", "remarks", "unique"))
+  gradebook <- subset(gradebook, select=c("Student", "SIS.User.ID", "letter", "absences", "remarks", "unique"))
   #rename to match registrar requirements
   colnames(gradebook)<- c("Name", "EID", "Grade", "Absences", "Remarks", "Unique")
   #export the results
   dir.create("Final Grades -- Registrar Formatting")
-  write.table(gradebook, file=paste(getwd(), "Final Grades -- Registrar Formatting", "FinalGradeSubmission.txt", sep="/"), sep="\t", row.names=F, na="")
-  write.csv(grades, file=paste(getwd(), "Final Grades -- Registrar Formatting", "Final Grades - Letters and Numbers.csv", sep="/"))
+  write.table(gradebook, file = paste(getwd(), "Final Grades -- Registrar Formatting", "FinalGradeSubmission.txt", sep="/"), sep = "\t", row.names = F, na = "")
+  write.csv(grades, file = paste(getwd(), "Final Grades -- Registrar Formatting", "Final Grades - Letters and Numbers.csv", sep = "/"))
 }
