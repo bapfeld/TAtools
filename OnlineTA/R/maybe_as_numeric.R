d1206b84581a83ff6735cb1c76d508568e349129
Brendan Apfeld <brendan.apfeld@utexas.edu>
2017-01-06 20:26:10 -0600

    Add Alex maybe_numeric function

---
+++ OnlineTA/R/maybe_as_numeric.R
@@ -0,0 +1,15 @@
+#' Alex's maybe_numeric function
+#'
+#' Internal function used to convert columns to numbers when they should be
+#'
+#' @param v A gradebook dataframe
+
+maybe_as_numeric <- function(v){
+  if (is(tryCatch(as.numeric(v),
+                  warning = function(w) w),
+         "warning")){
+    return(v)
+  } else{
+    return(as.numeric(v))
+  }
+}
