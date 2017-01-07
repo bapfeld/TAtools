90076c14c96df19ea70c9a40e591e1270e6ac166
Brendan Apfeld <brendan.apfeld@utexas.edu>
2017-01-06 21:27:15 -0600

    Lots of bug fixes

--- OnlineTA/R/final_grades_for_registrar.R
+++ OnlineTA/R/final_grades_for_registrar.R
@@ -13,46 +13,46 @@
 #' final.grades(gradebook.file, class="GOV312L", points=c(487, 462, 451, 430, 412, 400, 392, 386, 350, 330, 312), final = "points")
 #' @export
 
-final.grades <- function(gradebook, class = "GOV310L", non.enrolled = c("speede", "sm56684", "tm29778"), points = c(94, 90, 87, 84, 80, 77, 74, 70, 67, 64, 60), round = c("normal", "floor"), full.out = TRUE, final = c("points", "score")){
+final.grades <- function(gradebook, class = "GOV310L", non.enrolled = c("speede", "sm56684", "tm29778"), points = c(94, 90, 87, 84, 80, 77, 74, 70, 67, 64, 60), round = TRUE, drop_decimal = FALSE, full.out = TRUE, final = c("points", "score")){
+  #make sure final is set correctly
+  if(length(final) > 1){
+    stop("You must choose either 'points' or 'score' for the final variable.")
+  }
   #check length of points vector
   if (length(points)!=11) stop("Points vector not of length 11")
+  # clean the gb
+  clean_gb <- gradebook_clean(gradebook = gradebook, non_enrolled = non.enrolled)
+  gradebook <- clean_gb[[1]]
   #Are there any muted assignments? or the points possible row?
-  elim <- c()
-  if(mute_check(gradebook) == T){
+  if(clean_gb[[3]] == T){
     stop("Muted assignments detected - failed to export final grades.\n Muted assignments are not included in final grade calculations by Canvas. If you are confident your final points column is correct even with the muted assignments, you may ignore this message by deleting the muted assignment row (likely row 1) before you pass the gradebook to this function.")
   }
-  if(points_possible_check(gradebook) == T){
-    elim <- c(1)
-  }
 
-  #eliminate non-enrolled students and other top junk
-  elim_stud <- test.student(EIDs=non.enrolled, df=gradebook)
-  gradebook <- gradebook[-c(elim, elim_stud),]
   #column for unique number
   gradebook$unique <- as.numeric(substr(gradebook$Section, 9, 13))
 
-  #if gb was imported using check.names=F, remove space and add period
-  names(gradebook) <- gsub(" ", ".", names(gradebook))
-
   # Creating a vector of scores based on points or score
   if(final %in% c("points", "point", "Points", "Point", "p", "P")){
-    gradebook$final_num <- suppressWarnings(as.numeric(as.character(gradebook$Final.Points)))
+    gradebook$final_num <- gradebook$`Final Points`
   }else if(final %in% c("score", "scores", "Score", "score", "S", "s")){
-    gradebook$final_num <- suppressWarnings(as.numeric(as.character(gradebook$Final.Score)))
+    gradebook$final_num <- gradebook$`Final Score`
   }else{
     stop("You must set parameter 'final' to either 'points' or 'score' to indicate which column to use to assign final grades.\n Final grades not exported.")
   }
 
    ## Dealing with grades
   points <- sort(c(points, Inf, 0), decreasing=F)
-  if(round %in% c("floor", "Floor", "f", "F")){
-    gradebook$Grade_Based_on_Points <- floor(gradebook$final_num)
+  if(drop_decimal == TRUE){
+    gradebook$Grade_Based_on_Points <- as.numeric(substr(gradebook$`Final Points`, 1,3))
     gradebook$letter <- num_to_letter(gradebook$Grade_Based_on_Points, cutpoints = points, round = F)
-  } else if(round %in% c("normal", "Normal", "n", "N")){
-    gradebook$Grade_Based_on_Points <- round(gradebook$final_num)
+  }
+
+  if(round == TRUE){
+    gradebook$Grade_Based_on_Points <- round(as.numeric(gradebook$`Final Points`))
     gradebook$letter <- num_to_letter(gradebook$Grade_Based_on_Points, cutpoints = points, round = T)
   } else{
-    stop("It appears that you forgot to set the round variable. Please set it to either 'normal' for noraml mathematical rounding of grades, or 'floor' to use only the whole number.")
+    gradebook$Grade_Based_on_Points <- as.numeric(gradebook$`Final Points`)
+    gradebook$letter <- num_to_letter(gradebook$Grade_Based_on_Points, cutpoints = points, round = F)
   }
 
   #Add columns for absences & remarks
@@ -63,11 +63,11 @@ final.grades <- function(gradebook, class = "GOV310L", non.enrolled = c("speede"
   if (full.out == TRUE){
     grades <- gradebook
   } else if(final %in% c("points", "point", "Points", "Point", "p", "P")){
-    grades <- subset(gradebook, select = c("Student", "SIS.User.ID", "letter", "absences", "remarks", "unique", "Final.Points"))
+    grades <- subset(gradebook, select = c("Student", "SIS User ID", "letter", "absences", "remarks", "unique", "Final Points"))
   } else{
-    grades <- subset(gradebook, select = c("Student", "SIS.User.ID", "letter", "absences", "remarks", "unique", "Final.Score"))
+    grades <- subset(gradebook, select = c("Student", "SIS User ID", "letter", "absences", "remarks", "unique", "Final.Score"))
   }
-  gradebook <- subset(gradebook, select=c("Student", "SIS.User.ID", "letter", "absences", "remarks", "unique"))
+  gradebook <- subset(gradebook, select=c("Student", "SIS User ID", "letter", "absences", "remarks", "unique"))
   #rename to match registrar requirements
   colnames(gradebook)<- c("Name", "EID", "Grade", "Absences", "Remarks", "Unique")
   #export the results
